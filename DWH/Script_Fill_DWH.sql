0cac96a73374dc0d9e30c4d9250b452d33514f35
Jonas De Reuse <74045267+jokicker@users.noreply.github.com>
2023-04-26 10:48:49 +0200

    Afgewerkt, moet enkel nog getest worden

--- DWH/Script_Fill_DWH.sql
+++ DWH/Script_Fill_DWH.sql
@@ -86,22 +86,22 @@ DELIMITER ;
 -- Fill up the dim_date table
 CALL fill_dim_date();
 
--- Update dim_airline
-UPDATE Dim_Airline da SET airline_name = (SELECT airline_name FROM vluchten.airlines WHERE vluchten.airlines.airline_id = da.airline_id);
+-- Update dim_airline when namechange
+UPDATE Dim_Airline da SET airline_name = (SELECT airline_name FROM airfares.airlines WHERE airfares.airlines.airline_iata_code = da.airline_iata_code);
 
 -- Insert new records into Dim_Airline
-INSERT INTO Dim_Airline(airline_id, airline_name)
-SELECT DISTINCT airline_id, airline_name FROM vluchten.airlines WHERE airline_id NOT IN (SELECT DISTINCT airline_id FROM Dim_Airline);
+INSERT INTO Dim_Airline(airline_iata_code, airline_name)
+SELECT DISTINCT airline_iata_code, airline_name FROM airfares.airlines WHERE airline_iata_code NOT IN (SELECT DISTINCT airline_iata_code FROM Dim_Airline);
 
--- Update dim_airport
-UPDATE Dim_Airport da SET airport_name = (SELECT airport_name FROM vluchten.airports WHERE vluchten.airports.airport_id = da.airport_id);
+-- Update dim_airport when namechange
+UPDATE Dim_Airport da SET airport_name = (SELECT airport_name FROM airfares.airports WHERE airfares.airports.airport_iata_code = da.airport_iata_code);
 
 -- Insert new records into Dim_Airport
-INSERT INTO Dim_Airport(airport_id, airport_name, airport_code, city, country)
-SELECT DISTINCT airport_id, airport_name, airport_code, city, country FROM vluchten.airports WHERE airport_id NOT IN (SELECT DISTINCT airport_id FROM Dim_Airline);
+INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country)
+SELECT DISTINCT airport_iata_code, airport_name, place, country FROM airfares.airports WHERE airport_iata_code NOT IN (SELECT DISTINCT airport_iata_code FROM Dim_Airline);
 
--- Create trigger for inserting data in Dim_Flight, if a certain flight_key does not exist yet or one of it's other
--- attributes has changed, it updates the old data's end_date and fact_flight's flight_key
+-- Create trigger for inserting data in Dim_Flight, if a certain flight_key does not exist yet, it does nothing, if one of it's
+-- attributes has changed, it updates the old data's end_date
 DELIMITER $$
 
 CREATE TRIGGER update_end_date_dim_flight BEFORE INSERT ON Dim_Flight
@@ -109,35 +109,25 @@ FOR EACH ROW
 BEGIN
 DECLARE end_date_var DATE;
 SET end_date_var = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
-IF EXISTS (SELECT 1 FROM dim_flight WHERE (end_date IS NULL AND flight_key = NEW.flight_key AND (arrival_time <> NEW.arrival_time OR departure_time <> NEW.departure_time OR flight_duration <> NEW.flight_duration OR layovers <> NEW.layovers OR available_seats <> NEW.available_seats OR price <> NEW.price))) THEN
-UPDATE dim_flight df SET end_date = end_date_var WHERE (end_date IS NULL AND flight_key = NEW.flight_key);
-UPDATE fact_flight SET flight_key = max(df.flight_id)+1;
+IF EXISTS (SELECT 1 FROM dim_flight WHERE (end_date IS NULL AND flight_code = NEW.flight_code AND (arrival_time <> NEW.arrival_time OR departure_time <> NEW.departure_time OR flight_duration <> NEW.flight_duration OR layovers <> NEW.layovers OR available_seats <> NEW.available_seats OR price <> NEW.price))) THEN
+UPDATE dim_flight df SET end_date = end_date_var WHERE (end_date IS NULL AND flight_code = NEW.flight_code);
 END IF;
 END$$
 
 DELIMITER ;
 
--- Insert new records into Dim_Flight
-CREATE TEMPORARY TABLE temp_table(
-  airline_name varchar(255) NOT NULL,
-  departure_airport_code varchar(10) NOT NULL,
-  arrival_airport_code varchar(10) NOT NULL,
-  flight_key varchar(255) NOT NULL,
-  departure_date DATE NOT NULL,
-  arrival_date DATE NOT NULL,
-  scrape_date DATE NOT NULL
-  );
-
-
--- FROM HERE ON THINGS WILL NOT WORK
-
 -- Inserts new records into Dim_Flight
-INSERT INTO Dim_Flight(flight_key, arrival_time, departure_time, flight_duration, flight_number, layovers, available_seats, price, start_date) (SELECT fl.flight_key, fl.arrival_time, fl.departure_time, fl.flight_duration, fl.layovers, sd.seats_available, sd.price, sd.search_date FROM vluchten.flights fl JOIN vluchten.search_dates sd ON fl.flight_key = sd.flight_key WHERE fl.flight_key NOT IN (SELECT flight_key FROM dim_flight));
-INSERT INTO temp_table;
+INSERT INTO Dim_Flight(flight_code, flight_number, arrival_time, departure_time, flight_duration, layovers, available_seats, price, start_date) SELECT fl.flight_id, fl.flightnumber, fl.arrival_time, fl.departure_time, fl.duration, fl.number_of_stops, sd.seats_available, sd.price, sd.scrape_date FROM airfares.flights fl JOIN airfares.search_dates sd ON fl.flight_id = sd.flight_id WHERE (fl.flight_id NOT IN (SELECT flight_id FROM dim_flight) OR EXISTS (SELECT 1 FROM dim_flight WHERE (end_date IS NULL AND flight_id = NEW.flight_id AND (arrival_time <> NEW.arrival_time OR departure_time <> NEW.departure_time OR flight_duration <> NEW.flight_duration OR layovers <> NEW.layovers OR available_seats <> NEW.available_seats OR price <> NEW.price))));
 
--- Inserts changed previously existing records into Dim_Flight
-INSERT INTO Dim_Flight SELECT fl.flight_key, fl.arrival_time, fl.departure_time, fl.flight_duration, fl.layovers, sd.seats_available, sd.price, sd.search_date FROM dim_flight df JOIN vluchten.flights fl ON df.flight_key = fl.flight_key JOIN vluchten.search_dates sd ON fl.flight_key = sd.flight_key WHERE fl.flight_key = df.flight_key AND (df.arrival_time <> fl.arrival_time OR df.departure_time <> fl.departure_time OR df.flight_duration <> fl.flight_duration OR df.layovers <> fl.layovers OR df.available_seats <> sd.available_seats OR df.price <> sd.price);
-INSERT INTO temp_table;
-
--- insert new records into Fact_Flight
-INSERT INTO Fact_Flight SELECT tt.airline_key, tt.dep_airport_key, tt.arr_airport_key, tt.flight_key, scrape_date_key, departure_date_key, arrival_date_key FROM dim_airline airl, dim_airport airp, dim_date dd, dim_flight df, temp_table tt JOIN vluchten.airlines vairl WHERE 
+-- Insert new records into Fact_Flight
+INSERT INTO Fact_Flight(airline_key, dep_airport_key, arr_airport_key, flight_key, scrape_date_key, departure_date_key, arrival_date_key) 
+SELECT da.airline_id, das.airport_id, dad.airport_id, df.flight_id, dd1.date_key, dd2.date_key, dd3.date_key
+FROM airfares.flight af 
+	JOIN airfares.search_dates asd ON af.flight_id = asd.flight_id
+	JOIN dim_airline da ON af.airline_iata_code = da.airline_iata_code
+	JOIN dim_airport das ON af.departure_airport_iata_code = das.airport_iata_code
+    JOIN dim_airport dad ON af.departure_airport_iata_code = dad.airport_iata_code
+    JOIN dim_flight df ON af.flight_id = df.flight_code
+    JOIN dim_date dd1 ON asd.scrape_date = dd1.fullDate
+    JOIN dim_date dd2 ON fl.departure_date = dd2.fullDate
+    JOIN dim_date dd3 ON fl.arrival_date = dd3.fullDate
