21ea3415bcf0bdd624ba56ef988dfc4e7cdbc731
Jonas De Reuse <74045267+jokicker@users.noreply.github.com>
2023-05-04 23:01:46 +0200

    final Draft

--- DWH/Script_Create_DWH.sql
+++ DWH/Script_Create_DWH.sql
@@ -27,66 +27,53 @@ CREATE TABLE IF NOT EXISTS Dim_Flight (
   departure_time DATETIME NOT NULL,
   flight_duration int NOT NULL,
   layovers INT NOT NULL,
-  available_seats INT NOT NULL,
-  price DOUBLE NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Dim_Date (
-  date_key INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  fullDate DATE NOT NULL,
-  dayNumberOfWeek INT NOT NULL,
-  isHoliday BOOLEAN NOT NULL,
+  date_id INT PRIMARY KEY,
+  fullDate DATE,
+  dayNumberOfWeek INT,
+  nameWeekDay VARCHAR(10),
+  numberOfQuarter INT,
+  isHoliday BOOLEAN,
   nameHoliday VARCHAR(50),
-  isWeekend BOOLEAN NOT NULL
+  isWeekend BOOLEAN,
+  UNIQUE (date_id)
 );
 
 CREATE TABLE IF NOT EXISTS Fact_Flight (
-factFlight_key INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-airline_key INT NOT NULL,
-dep_airport_key INT NOT NULL,
-arr_airport_key INT NOT NULL,
-flight_key INT NOT NULL,
-scrape_date_key INT NOT NULL,
-departure_date_key INT NOT NULL,
-arrival_date_key INT NOT NULL,
+factFlight_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+airline_id INT NOT NULL,
+dep_airport_id INT NOT NULL,
+arr_airport_id INT NOT NULL,
+flight_id INT NOT NULL,
+scrape_date_id INT NOT NULL,
+departure_date_id INT NOT NULL,
+arrival_date_id INT NOT NULL,
+available_seats INT NOT NULL,
+price DOUBLE NOT NULL,
 
 CONSTRAINT FK_Airline_Key
-FOREIGN KEY (airline_key)
+FOREIGN KEY (airline_id)
 REFERENCES AirFaresDWH.Dim_Airline(airline_id),
 CONSTRAINT FK_Dep_airport_Key
-FOREIGN KEY (dep_airport_key)
+FOREIGN KEY (dep_airport_id)
 REFERENCES AirFaresDWH.Dim_Airport(airport_id),
 CONSTRAINT FK_Arr_airport_Key
-FOREIGN KEY (arr_airport_key)
+FOREIGN KEY (arr_airport_id)
 REFERENCES AirFaresDWH.Dim_Airport(airport_id),
 CONSTRAINT FK_Flight_Key
-FOREIGN KEY (flight_key)
+FOREIGN KEY (flight_id)
 REFERENCES AirFaresDWH.Dim_Flight(flight_id),
 CONSTRAINT FK_Scrape_Date_Key
-FOREIGN KEY (scrape_date_key)
-REFERENCES AirFaresDWH.Dim_Date(date_key),
+FOREIGN KEY (scrape_date_id)
+REFERENCES AirFaresDWH.Dim_Date(date_id),
 CONSTRAINT FK_Departure_Date_Key
-FOREIGN KEY (departure_date_key)
-REFERENCES AirFaresDWH.Dim_Date(date_key),
+FOREIGN KEY (departure_date_id)
+REFERENCES AirFaresDWH.Dim_Date(date_id),
 CONSTRAINT FK_Arrival_Date_Key
-FOREIGN KEY (arrival_date_key)
-REFERENCES AirFaresDWH.Dim_Date(date_key)
+FOREIGN KEY (arrival_date_id)
+REFERENCES AirFaresDWH.Dim_Date(date_id)
 );
-
--- filling up predetermined columns
-
-LOCK TABLES Dim_Airline WRITE;
-INSERT INTO Dim_Airline(airline_iata_code, airline_name) VALUES ('FR','Ryanair'),('TB','TUI fly'),('HV', 'Transavia'),('SN','Brussels Airlines');
-UNLOCK TABLES;
-
-LOCK TABLES Dim_Airport WRITE;
-INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country) VALUES ('BRU','Brussels Airports','Zaventem','Belgium'),('ANR','Antwerp International Airport','Deurne','Belgium'),('LGG','Liege Airport','Luik','Belgium'),
-('CRL','Brussels South Charleroi Airport','Charleroi','Belgium'),('OST','Ostend Bruges International Airport','Oostende','Belgium');
-INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country) VALUES ('HER','Heraklion International Airport','Crete','Greece'),('CFU','Corfu International Airport','Corfu','Greece'),('RHO','Rhodes International Airport','Rhodes','Greece');
-INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country) VALUES ('PMO','Palermo Falcone-Borsellino Airport','Palermo','Italy'),('NAP','Naples Airport','Naples','Italy'),
-('BDS','Brindisi Airport','Brindisi','Italy'),('FAo','Faro Airport','Faro','Portugal');
-INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country) VALUES ('ALC','Alicante Airport','Alicante','Spain'),('AGP','Malaga Costa Del Sol Airport','Malaga','Spain'),('IBZ','Ibiza Airport','Ibiza','Spain'),
-('PMI','Palma de Mallorca Airport','Palma','Spain'),('TFS','Tenerife South Airport','Tenerife','Spain');
-UNLOCK TABLES;
