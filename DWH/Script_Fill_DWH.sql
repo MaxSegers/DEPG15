6fe5e38164488a7b579b7c0c77d0c7d34eaaaed7
Jonas De Reuse <74045267+jokicker@users.noreply.github.com>
2023-05-04 23:01:03 +0200

    Update Script_Fill_DWH.sql
    moeten enkel nog een paar kolommen bij in Dim_date, voor de rest zou alles moeten werken

--- DWH/Script_Fill_DWH.sql
+++ DWH/Script_Fill_DWH.sql
@@ -11,76 +11,56 @@ DELETE FROM dim_date;
 
 -- Recreates the fill_dim_date procedure which fills the dim_date table
 DROP PROCEDURE IF EXISTS fill_dim_date;
-DELIMITER //
 
+DELIMITER //
 CREATE PROCEDURE fill_dim_date()
 BEGIN
-    DECLARE start_date DATE;
-    DECLARE end_date DATE;
-    DECLARE curr_date DATE;
-    DECLARE current_day_of_week INT;
-    DECLARE current_month INT;
-    DECLARE current_day INT;
-    DECLARE current_year INT;
-    DECLARE is_holiday BOOLEAN;
-    DECLARE holiday_name VARCHAR(50);
-    DECLARE is_weekend BOOLEAN;
+    DECLARE start_date DATE DEFAULT '2023-01-01';
+    DECLARE end_date DATE DEFAULT '2023-12-31';
+    DECLARE curr_date DATE DEFAULT start_date;
 
-    SET start_date = '2023-01-01';
-    SET end_date = '2023-12-31';
-    SET curr_date = start_date;
-
+    -- Fill up the Dim_Date table
     WHILE curr_date <= end_date DO
-        SET current_day_of_week = DAYOFWEEK(curr_date);
-        SET current_month = MONTH(curr_date);
-        SET current_day = DAY(curr_date);
-        SET current_year = YEAR(curr_date);
-        SET is_holiday = FALSE;
-        SET holiday_name = '';
-        SET is_weekend = (current_day_of_week = 1 OR current_day_of_week = 7);
-
-        -- Check for holidays in Belgium
-        IF current_month = 1 AND current_day = 1 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'New Year''s Day';
-        ELSEIF current_month = 4 AND current_day = 17 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Easter Monday';
-        ELSEIF current_month = 5 AND current_day = 1 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Labour Day';
-        ELSEIF current_month = 5 AND current_day = 25 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Ascension Day';
-        ELSEIF current_month = 6 AND current_day = 5 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Whit Monday';
-        ELSEIF current_month = 7 AND current_day = 21 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Belgian National Day';
-        ELSEIF current_month = 8 AND current_day = 15 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Assumption of Mary';
-        ELSEIF current_month = 11 AND current_day = 1 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'All Saints'' Day';
-        ELSEIF current_month = 11 AND current_day = 11 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Armistice Day';
-        ELSEIF current_month = 12 AND current_day = 25 THEN
-            SET is_holiday = TRUE;
-            SET holiday_name = 'Christmas Day';
-        END IF;
-
-        -- Insert the row into the dim_date table
-        INSERT INTO dim_date (fullDate, dayNumberOfWeek, isHoliday, nameHoliday, isWeekend)
-        VALUES (curr_date, current_day_of_week, is_holiday, holiday_name, is_weekend);
-
-        -- Increment the curr_date variable
-        SET curr_date = DATE_ADD(curr_date, INTERVAL 1 DAY);
+        INSERT INTO Dim_Date (date_id, fullDate, dayNumberOfWeek, nameWeekDay, numberOfQuarter, isHoliday, nameHoliday, isWeekend)
+        VALUES (
+            CONCAT(YEAR(curr_date), LPAD(MONTH(curr_date), 2, '0'), LPAD(DAY(curr_date), 2, '0')),
+            curr_date,
+            (DAYOFWEEK(curr_date) + 5) % 7,
+            CASE WEEKDAY(curr_date)
+				WHEN 0 THEN 'Monday'
+				WHEN 1 THEN 'Tuesday'
+				WHEN 2 THEN 'Wednesday'
+				WHEN 3 THEN 'Thursday'
+				WHEN 4 THEN 'Friday'
+				WHEN 5 THEN 'Saturday'
+				WHEN 6 THEN 'Sunday'
+			END,
+            QUARTER(curr_date),
+            CASE 
+                WHEN MONTH(curr_date) = 1 AND DAY(curr_date) = 1 THEN TRUE
+                WHEN MONTH(curr_date) = 5 AND DAY(curr_date) = 1 THEN TRUE
+                WHEN MONTH(curr_date) = 7 AND DAY(curr_date) = 21 THEN TRUE
+                WHEN MONTH(curr_date) = 11 AND DAY(curr_date) = 1 THEN TRUE
+                WHEN MONTH(curr_date) = 12 AND (DAY(curr_date) = 25 OR DAY(curr_date) = 26) THEN TRUE
+                ELSE FALSE
+            END,
+            CASE 
+                WHEN MONTH(curr_date) = 1 AND DAY(curr_date) = 1 THEN 'New Year\'s Day'
+                WHEN MONTH(curr_date) = 5 AND DAY(curr_date) = 1 THEN 'Labour Day'
+                WHEN MONTH(curr_date) = 7 AND DAY(curr_date) = 21 THEN 'Belgian National Day'
+                WHEN MONTH(curr_date) = 11 AND DAY(curr_date) = 1 THEN 'All Saints\' Day'
+                WHEN MONTH(curr_date) = 12 AND DAY(curr_date) = 25 THEN 'Christmas Day'
+                WHEN MONTH(curr_date) = 12 AND DAY(curr_date) = 26 THEN 'Boxing Day'
+                ELSE NULL
+            END,
+            CASE 
+                WHEN (DAYOFWEEK(curr_date) + 5) % 7 IN (5, 6) THEN TRUE
+                ELSE FALSE
+            END
+        );
+        SET curr_date := DATE_ADD(curr_date, INTERVAL 1 DAY);
     END WHILE;
-END //
-
+END//
 DELIMITER ;
 
 -- Fill up the dim_date table
@@ -98,36 +78,55 @@ UPDATE Dim_Airport da SET airport_name = (SELECT airport_name FROM airfares.airp
 
 -- Insert new records into Dim_Airport
 INSERT INTO Dim_Airport(airport_iata_code, airport_name, city, country)
-SELECT DISTINCT airport_iata_code, airport_name, place, country FROM airfares.airports WHERE airport_iata_code NOT IN (SELECT DISTINCT airport_iata_code FROM Dim_Airline);
+SELECT DISTINCT airport_iata_code, airport_name, place, country FROM airfares.airports WHERE airport_iata_code NOT IN (SELECT DISTINCT airport_iata_code FROM Dim_Airport);
 
 -- Create trigger for inserting data in Dim_Flight, if a certain flight_key does not exist yet, it does nothing, if one of it's
 -- attributes has changed, it updates the old data's end_date
 DELIMITER $$
 
-CREATE TRIGGER update_end_date_dim_flight BEFORE INSERT ON Dim_Flight
-FOR EACH ROW
-BEGIN
-DECLARE end_date_var DATE;
-SET end_date_var = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
-IF EXISTS (SELECT 1 FROM dim_flight WHERE (end_date IS NULL AND flight_code = NEW.flight_code AND (arrival_time <> NEW.arrival_time OR departure_time <> NEW.departure_time OR flight_duration <> NEW.flight_duration OR layovers <> NEW.layovers OR available_seats <> NEW.available_seats OR price <> NEW.price))) THEN
-UPDATE dim_flight df SET end_date = end_date_var WHERE (end_date IS NULL AND flight_code = NEW.flight_code);
-END IF;
-END$$
+-- Inserts new records with flight_code that does not exist yet in table into Dim_Flight
+INSERT INTO Dim_Flight(flight_code, flight_number, arrival_time, departure_time, flight_duration, layovers, start_date) SELECT fl.flight_id, fl.flightnumber, fl.arrival_time, fl.departure_time, fl.duration, fl.number_of_stops, sd.scrape_date FROM airfares.flights fl JOIN airfares.search_dates sd ON fl.flight_id = sd.flight_id WHERE (fl.flight_id NOT IN (SELECT flight_code FROM dim_flight));
 
-DELIMITER ;
+-- Create a temp_table to store flights that have changed
+CREATE TEMPORARY TABLE temp_table (
+  flight_code varchar(255) NOT NULL,
+  flight_number varchar(10) NOT NULL,
+  arrival_time DATETIME NOT NULL,
+  departure_time DATETIME NOT NULL,
+  flight_duration int NOT NULL,
+  layovers INT NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE
+);
 
--- Inserts new records into Dim_Flight
-INSERT INTO Dim_Flight(flight_code, flight_number, arrival_time, departure_time, flight_duration, layovers, available_seats, price, start_date) SELECT fl.flight_id, fl.flightnumber, fl.arrival_time, fl.departure_time, fl.duration, fl.number_of_stops, sd.seats_available, sd.price, sd.scrape_date FROM airfares.flights fl JOIN airfares.search_dates sd ON fl.flight_id = sd.flight_id WHERE (fl.flight_id NOT IN (SELECT flight_id FROM dim_flight) OR EXISTS (SELECT 1 FROM dim_flight WHERE (end_date IS NULL AND flight_id = NEW.flight_id AND (arrival_time <> NEW.arrival_time OR departure_time <> NEW.departure_time OR flight_duration <> NEW.flight_duration OR layovers <> NEW.layovers OR available_seats <> NEW.available_seats OR price <> NEW.price))));
+-- Insert records that previously existed into temp_table
+INSERT INTO Dim_Flight(flight_code, flight_number, arrival_time, departure_time, flight_duration, layovers, start_date) 
+	SELECT fl.flight_id, fl.flightnumber, fl.arrival_time, fl.departure_time, fl.duration, fl.number_of_stops, sd.scrape_date 
+    FROM airfares.flights fl 
+    JOIN dim_flight df ON fl.flight_id = df.flight_code
+    WHERE (fl.duration <> df.flight_duration OR fl.arrival_time <> df.arrival_time OR fl.departure_time <> df.departure_time OR fl.number_of_stops <> df.layovers) AND (df.end_date IS NULL);
+
+-- Update end_date in dim_flight
+UPDATE Dim_Flight SET end_date = subdate(current_date, 1) WHERE flight_code IN (SELECT flight_code FROM temp_table)
+
+-- Insert temp_table into dim_flight
+INSERT INTO Dim_Flight(flight_code, flight_number, arrival_time, departure_time, flight_duration, layovers, start_date) SELECT * FROM temp_table;
 
 -- Insert new records into Fact_Flight
-INSERT INTO Fact_Flight(airline_key, dep_airport_key, arr_airport_key, flight_key, scrape_date_key, departure_date_key, arrival_date_key) 
-SELECT da.airline_id, das.airport_id, dad.airport_id, df.flight_id, dd1.date_key, dd2.date_key, dd3.date_key
+INSERT INTO Fact_Flight(airline_id, dep_airport_id, arr_airport_id, flight_id, scrape_date_id, departure_date_id, arrival_date_id, available_seats, price) 
+SELECT da.airline_id, das.airport_id, dad.airport_id, df.flight_id, dd1.date_id, dd2.date_id, dd3.date_id, asd.seats_available, asd.price
 FROM airfares.flight af 
 	JOIN airfares.search_dates asd ON af.flight_id = asd.flight_id
 	JOIN dim_airline da ON af.airline_iata_code = da.airline_iata_code
 	JOIN dim_airport das ON af.departure_airport_iata_code = das.airport_iata_code
-    JOIN dim_airport dad ON af.departure_airport_iata_code = dad.airport_iata_code
+    JOIN dim_airport dad ON af.arrival_airport_iata_code = dad.airport_iata_code
     JOIN dim_flight df ON af.flight_id = df.flight_code
     JOIN dim_date dd1 ON asd.scrape_date = dd1.fullDate
-    JOIN dim_date dd2 ON fl.departure_date = dd2.fullDate
-    JOIN dim_date dd3 ON fl.arrival_date = dd3.fullDate
+    JOIN dim_date dd2 ON af.departure_date = dd2.fullDate
+    JOIN dim_date dd3 ON af.arrival_date = dd3.fullDate
+WHERE 
+/* Slowly Changing Dimension dimCustomer */
+asd.scrape_date >= df.start_date and (df.end_date is null or asd.scrape_date <= df.end_date) 
+AND
+/* only add new lines + make sure it runs from an empty FactSales table */
+asd.flight_id > (SELECT IFNULL(MAX(factFlight_id),0) from Fact_Flight)
